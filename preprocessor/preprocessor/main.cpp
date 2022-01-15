5e8ddec5cc9eb8faf9b300dc5e866e7127c5b028
grafon100 <grafon100@hotmail.com>
2022-01-15 12:29:26 +0100

    Preprocessor #define

--- preprocessor/preprocessor/main.cpp
+++ preprocessor/preprocessor/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
+#define PI 3.14
+//better didn't use #define begin {, #define end }
+//this code for fun
+#define begin {
+#define end }
+
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    for(int i = 0; i<5; i++)
+    begin
+        
+        double result = PI + i;
+        cout<<i+1<<") ";
+        cout<<PI<<"+"<<i<<"="<<result<<endl;
+        
+    end
+    
     return 0;
 }
